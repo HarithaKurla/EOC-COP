281fd3e533b661f77dba4b0893fbff95ce5e6d5d
Kurla <S528744@nwmissouri.edu>
2017-11-15 08:59:29 -0600

    Added functionality for sending mail in forgot password using sendgrid

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 
 const path = require("path");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
+var bodyparser = require ("body-parser");
+
 
 
 // =========================================================
@@ -12,7 +13,7 @@ const bodyParser = require("body-parser");
 // create express app 
 global.app = express();
 
-
+app.use(bodyparser.urlencoded({extended:false}));
 // set the root view folder & specify the view engine 
 app.set("views", path.join(__dirname, "views"));
 //app.engine('ejs', engines.ejs);
@@ -62,17 +63,18 @@ app.get("/route",function (request, response) {
 
 // 5 handle an http POST request to the new-entry URI 
 app.post("/forgetpassword", function (request, response) {
-	console.log(request.query.email);
+	//console.log(request.body);
+	//console.log(request.query);
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 's528744@nwmissouri.edu',
+  to: request.body.email,
   from: 'noreply@example.com',
   subject: 'Password Recovery mail',
   text: 'Hai your new password is abc123'
 };
 sgMail.send(msg);
-response.send("sample.ejs","email has been sent");
+//response.send("sample.ejs","email has been sent");
 response.render("forgetpassword.ejs");
 });
 
